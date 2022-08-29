efd1c11f4997a613397d83d712940a5a0fd3d480
detailswes <details.wes@gmail.com>
2022-08-29 15:38:53 +0530

    testing website

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Routes, Route, Link } from "react-router-dom"
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div className="App">
+      <Link to="/">home</Link>
+      <Link to="/about" >about</Link>
+      <br></br>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
+
+
+const Home = () => {
+  return (
+    <>
+      home page
+    </>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      about js
+    </>
+  )
+}
+
+
+
