909145a4918f7e8600fbbb78f826465a377b15b4
detailswes <details.wes@gmail.com>
2022-08-29 18:18:37 +0530

    Change the origin url.

--- src/view/Login/index.jsx
+++ src/view/Login/index.jsx
@@ -4,6 +4,7 @@ export default function Login() {
    return (
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-login h-screen pt-16 text-center relative">
          <div>
+
             <img src="src/assets/images/splashicon.svg" className="m-auto w-12" />
             <h3 className="pt-3 text-pink-600 text-2xl font-bold">Finance <span className="text-violet-700">Manager</span></h3>
             <h2 className="pt-6 text-3xl font-bold">Seamlessly manage your Finances</h2>
