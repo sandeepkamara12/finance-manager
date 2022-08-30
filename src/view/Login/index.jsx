4ec319545a29480ab5e8cf61a22b2daa00de8d1c
detailswes <details.wes@gmail.com>
2022-08-30 14:16:13 +0530

    update the image for google

--- src/view/Login/index.jsx
+++ src/view/Login/index.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import FinanceManager from "../../assets/images/finance-manager.png";
 import SplashIcon from "../../assets/images/splashicon.svg";
-
+import Google from '../../assets/images/google.png';
 export default function Login() {
    return (
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-login h-screen pt-16 text-center relative">
@@ -12,7 +12,7 @@ export default function Login() {
             <h2 className="pt-6 text-3xl font-bold">Seamlessly manage your Finances</h2>
             <p className="pt-3 text-slate-600 mb-3">Discover the revolutionary semi automated financial management experience.</p>
             {/* <a href=""><img src="src/assets/images/google.png" />Continue with Google</a> */}
-            <Link to='/' className='inline-flex items-center py-3 px-6 bg-white rounded-3xl mt-5 font-bold text-sm'><img src="src/assets/images/google.png" className='mr-2' />Continue with Google</Link>
+            <Link to='/' className='inline-flex items-center py-3 px-6 bg-white rounded-3xl mt-5 font-bold text-sm'><img src={Google} className='mr-2' />Continue with Google</Link>
             <div className='financemanager_image absolute w-full bottom-0 left-5'>
                <img src={FinanceManager} />
             </div>
