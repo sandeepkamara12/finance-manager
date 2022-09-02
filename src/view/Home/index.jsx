10f03e641a5f411e7308485590f87cad956c62df
detailswes <details.wes@gmail.com>
2022-09-02 17:37:01 +0530

    hide the keyboard.

--- src/view/Home/index.jsx
+++ src/view/Home/index.jsx
@@ -44,7 +44,7 @@ export default function Home() {
                         <FontAwesomeIcon icon={faAngleLeft} size="lg" />Aug 2022<FontAwesomeIcon icon={faAngleRight} size="lg" />
                      </h4>
                      <div className='flex items-center justify-between relative w-2/5'>
-                        <Select value={data.filter(obj => obj.value === selectedValue)} blurInputOnSelect={false} styles={customStyles} onChange={handleChange} options={data} name="paymentType" id="paymentType" className='text-16 leading-20 text-black font-medium w-full' />
+                        <Select value={data.filter(obj => obj.value === selectedValue)} isSearchable={false} styles={customStyles} onChange={handleChange} options={data} name="paymentType" id="paymentType" className='text-16 leading-20 text-black font-medium w-full' />
                      </div>
                   </div>
                   <div className="meta-info flex flex-wrap items-center justify-between mt-4">
