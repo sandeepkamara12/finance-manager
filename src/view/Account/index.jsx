12a8267ab2c5110f75897390d505280651f35d15
detailswes <details.wes@gmail.com>
2022-09-05 15:57:46 +0530

    update the account screen.

--- src/view/Account/index.jsx
+++ src/view/Account/index.jsx
@@ -10,63 +10,63 @@ export default function Account() {
       <Layout>
          <div className="h-screen">
             <div className="add-expense add-expense adjust-scrollbar overflow-auto h-full ">
-               <div className="expense-form dashboard-header fixed top-0 left-0 right-0 bg-white">
+               <div className="expense-form dashboard-header fixed top-0 left-0 right-0 bg-white shadow-headerShadow">
                   <div className="expense-header border-b border-border">
-                     <h1 className='uppercase text-center font-semibold text-pink text-20 leading-24'>Account</h1>
+                     <h1 className='uppercase text-center font-semibold text-pink text-14 leading-18'>Account</h1>
                   </div>
                </div>
 
                <div className="category-wrap flex items-stretch justify-center flex-wrap px-5">
-                  <div className="category w-1/2 flex flex-wrap items-center justify-center flex-column text-center pb-6">
-                     <div className="bg-profile-gradient w-full rounded-lg py-3 flex flex-wrap items-center justify-center flex-column px-3 h-full">
+                  <div className="category w-1/2 flex flex-wrap items-center justify-center flex-column text-center pb-2">
+                     <div className="bg-profile-gradient w-full rounded py-3 flex flex-wrap items-center justify-center flex-column px-3 h-full">
                         <figure className='w-full'>
                            <NavLink to="/home">
-                              <img src={AccountProfile} alt="Profile" className='mx-auto' />
+                              <img src={AccountProfile} alt="Profile" className='mx-auto w-6' />
                            </NavLink>
                         </figure>
-                        <div className="pt-2 w-full text-white text-16 leading-26">
+                        <div className="w-full pt-2 text-white text-12 leading-16">
                            <NavLink to="/home">
                               Profile
                            </NavLink>
                         </div>
                      </div>
                   </div>
-                  <div className="category w-1/2 flex flex-wrap items-center justify-center flex-column text-center pb-6">
-                     <div className="bg-income-gradient w-full rounded-lg py-3 flex flex-wrap items-center justify-center flex-column px-3 h-full">
+                  <div className="category w-1/2 flex flex-wrap items-center justify-center flex-column text-center pb-2">
+                     <div className="bg-income-gradient w-full rounded py-3 flex flex-wrap items-center justify-center flex-column px-3 h-full">
                         <figure className='w-full'>
-                           <img src={AccountInExpense} alt="Profile" className='mx-auto' />
+                           <img src={AccountInExpense} alt="Profile" className='mx-auto w-6' />
                         </figure>
-                        <div className="pt-2 w-full text-white text-16 leading-26">Income Category</div>
+                        <div className="w-full pt-2 text-white text-12 leading-16">Income Category</div>
                      </div>
                   </div>
-                  <div className="category w-1/2 flex flex-wrap items-center justify-center flex-column text-center pb-6">
-                     <div className="bg-profile-gradient w-full rounded-lg py-3 flex flex-wrap items-center justify-center flex-column px-3 h-full">
+                  <div className="category w-1/2 flex flex-wrap items-center justify-center flex-column text-center pb-2">
+                     <div className="bg-profile-gradient w-full rounded py-3 flex flex-wrap items-center justify-center flex-column px-3 h-full">
                         <figure className='w-full'>
                            <NavLink to="/category-listing">
-                              <img src={AccountInExpense} alt="Profile" className='mx-auto' />
+                              <img src={AccountInExpense} alt="Profile" className='mx-auto w-6' />
                            </NavLink>
                         </figure>
-                        <div className="pt-2 w-full text-white text-16 leading-26">
+                        <div className="w-full pt-2 text-white text-12 leading-16">
                            <NavLink to="/category-listing">
                               Expense Category
                            </NavLink>
                         </div>
                      </div>
                   </div>
-                  <div className="category w-1/2 flex flex-wrap items-center justify-center flex-column text-center pb-6">
-                     <div className="bg-income-gradient w-full rounded-lg py-3 flex flex-wrap items-center justify-center flex-column px-3 h-full">
+                  <div className="category w-1/2 flex flex-wrap items-center justify-center flex-column text-center pb-2">
+                     <div className="bg-income-gradient w-full rounded py-3 flex flex-wrap items-center justify-center flex-column px-3 h-full">
                         <figure className='w-full'>
-                           <img src={Help} alt="Profile" className='mx-auto' />
+                           <img src={Help} alt="Profile" className='mx-auto w-6' />
                         </figure>
-                        <div className="pt-2 w-full text-white text-16 leading-26">Help</div>
+                        <div className="w-full pt-2 text-white text-12 leading-16">Help</div>
                      </div>
                   </div>
-                  <div className="category w-1/2 flex flex-wrap items-center justify-center flex-column text-center pb-6">
-                     <div className="bg-income-gradient w-full rounded-lg py-3 flex flex-wrap items-center justify-center flex-column px-3 h-full">
+                  <div className="category w-1/2 flex flex-wrap items-center justify-center flex-column text-center pb-2">
+                     <div className="bg-income-gradient w-full rounded py-3 flex flex-wrap items-center justify-center flex-column px-3 h-full">
                         <figure className='w-full'>
-                           <img src={Share} alt="Profile" className='mx-auto' />
+                           <img src={Share} alt="Profile" className='mx-auto w-6' />
                         </figure>
-                        <div className="pt-2 w-full text-white text-16 leading-26">Share</div>
+                        <div className="w-full pt-2 text-white text-12 leading-16">Share</div>
                      </div>
                   </div>
                </div>
