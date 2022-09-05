e70ee6842b0e158546258734967b219870f2feea
detailswes <details.wes@gmail.com>
2022-09-05 15:44:55 +0530

    update the add expense screen.

--- src/view/AddExpense/index.jsx
+++ src/view/AddExpense/index.jsx
@@ -41,77 +41,77 @@ export default function AddExpense() {
          <div className="h-screen bg-white">
             <div className="add-expense adjust-scrollbar overflow-auto h-full">
 
-               <div className="expense-form fixed top-0 left-0 right-0 bg-white dashboard-header">
+               <div className="expense-form fixed top-0 left-0 right-0 bg-white dashboard-header shadow-headerShadow">
                   <div className="expense-header border-b border-border">
-                     <h1 className='uppercase text-center font-semibold text-pink text-20 leading-24'>Add Expense</h1>
+                     <h1 className='uppercase text-center font-semibold text-pink text-14 leading-18'>Add Expense</h1>
                   </div>
                </div>
 
                <div className="buttons-wrap mb-6 flex items-center justify-center px-5">
-                  <button className='custom-btn rounded-xs border border-border uppercase text-14 leading-16 font-semibold inline-block mr-5'>income</button>
-                  <button className='custom-btn rounded-xs border border-pink uppercase text-14 leading-16 font-semibold inline-block text-pink'>expense</button>
+                  <button className='custom-btn rounded-xs border border-border uppercase text-12 leading-16 font-semibold inline-block mr-5'>income</button>
+                  <button className='custom-btn rounded-xs border border-pink uppercase text-12 leading-16 font-semibold inline-block text-pink'>expense</button>
                </div>
 
                <form onSubmit={formik.handleSubmit} className="px-5">
-                  <div className="field-wrap mb-3 flex flex-wrap items-center">
-                     <label htmlFor="date" className="text-16 leading-18 text-formLabel inline-block w-2/6">Date</label>
+                  <div className="field-wrap mb-2 flex flex-wrap items-center">
+                     <label htmlFor="date" className="text-12 leading-16 text-formLabel inline-block w-2/6">Date</label>
                      <input
                         id="date"
                         name="date"
                         type="date"
                         onChange={formik.handleChange}
                         value={formik.values.date}
-                        className="fields text-14 leading-18 text-black font-medium border-b border-border py-2 px-2 w-4/6 bg-transparent"
+                        className="fields text-12 leading-16 text-black font-medium border-b border-border py-1 px-2 w-4/6 bg-transparent"
                      />
                   </div>
-                  <div className="field-wrap mb-3 flex flex-wrap items-center">
-                     <label htmlFor="paymentType" className="text-16 leading-18 text-formLabel inline-block w-2/6">Account</label>
-                     {/* <Select options={options} onChange={selectHandler} name="paymentType" id="paymentType" className='fields text-14 leading-18 text-black font-medium border-b border-border py-3 px-2 w-4/6 inline-block' /> */}
+                  <div className="field-wrap mb-2 flex flex-wrap items-center">
+                     <label htmlFor="paymentType" className="text-12 leading-16 text-formLabel inline-block w-2/6">Account</label>
+                     {/* <Select options={options} onChange={selectHandler} name="paymentType" id="paymentType" className='fields text-12 leading-16 text-black font-medium border-b border-border py-1 px-2 w-4/6 inline-block' /> */}
                      <input
                         id="paymentType"
                         name="paymentType"
                         type="text"
                         onChange={formik.handleChange}
                         value={formik.values.paymentType}
-                        className='fields text-14 leading-18 text-black font-medium border-b border-border py-2 px-2 w-4/6 inline-block'
+                        className='fields text-12 leading-16 text-black font-medium border-b border-border py-1 px-2 w-4/6 inline-block'
                      />
                   </div>
-                  <div className="field-wrap mb-3 flex flex-wrap items-center">
-                     <label htmlFor="category" className="text-16 leading-18 text-formLabel inline-block w-2/6">Category</label>
+                  <div className="field-wrap mb-2 flex flex-wrap items-center">
+                     <label htmlFor="category" className="text-12 leading-16 text-formLabel inline-block w-2/6">Category</label>
                      <input
                         id="category"
                         name="category"
                         type="text"
                         onChange={formik.handleChange}
                         value={formik.values.category}
-                        className="fields text-14 leading-18 text-black font-medium border-b border-border py-2 px-2 w-4/6"
+                        className="fields text-12 leading-16 text-black font-medium border-b border-border py-1 px-2 w-4/6"
                      />
                   </div>
-                  <div className="field-wrap mb-3 flex flex-wrap items-center">
-                     <label htmlFor="amount" className="text-16 leading-18 text-formLabel inline-block w-2/6">Amount</label>
+                  <div className="field-wrap mb-2 flex flex-wrap items-center">
+                     <label htmlFor="amount" className="text-12 leading-16 text-formLabel inline-block w-2/6">Amount</label>
                      <input
                         id="amount"
                         name="amount"
                         type="number"
                         onChange={formik.handleChange}
                         value={formik.values.amount}
-                        className="fields text-14 leading-18 text-black font-medium border-b border-border py-2 px-2 w-4/6"
+                        className="fields text-12 leading-16 text-black font-medium border-b border-border py-1 px-2 w-4/6"
                      />
                   </div>
-                  <div className="field-wrap mb-3 flex flex-wrap items-center">
-                     <label htmlFor="note" className="text-16 leading-18 text-formLabel inline-block w-2/6">Note</label>
+                  <div className="field-wrap mb-2 flex flex-wrap items-center">
+                     <label htmlFor="note" className="text-12 leading-16 text-formLabel inline-block w-2/6">Note</label>
                      <input
                         id="note"
                         name="note"
                         type="text"
                         onChange={formik.handleChange}
                         value={formik.values.note}
-                        className="fields text-14 leading-18 text-black font-medium border-b border-border py-2 px-2 w-4/6"
+                        className="fields text-12 leading-16 text-black font-medium border-b border-border py-1 px-2 w-4/6"
                      />
                   </div>
                   <div className="buttons-wrap my-8 flex flex-wrap items-center justify-center">
-                     <button type="submit" className='custom-btn text-white bg-pink rounded-full text-14 leading-24 font-semibold mr-3'>Save</button>
-                     <button type="button" className='custom-btn text-white bg-purple rounded-full text-14 leading-24 font-semibold'>Save & Continue</button>
+                     <button type="submit" className='custom-btn text-white lower-btn bg-pink rounded-full text-12 leading-20 font-semibold mr-3'>Save</button>
+                     <button type="button" className='custom-btn text-white lower-btn bg-purple rounded-full text-12 leading-20 font-semibold'>Save & Continue</button>
                   </div>
                </form>
 
