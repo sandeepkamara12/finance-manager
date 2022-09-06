671edf258acb6a13e509f2af97ccc30790c11ff6
detailswes <details.wes@gmail.com>
2022-09-06 11:30:37 +0530

    do some updates for PWA.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
+    mode: "development",
     devOptions: {
       enabled: true
     }
