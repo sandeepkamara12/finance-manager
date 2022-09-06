aa42212b33633e0b289d4e61b008c61f5fe62053
detailswes <details.wes@gmail.com>
2022-09-06 10:19:17 +0530

    stop the zoom effect and update the login screen.

--- src/view/Login/index.jsx
+++ src/view/Login/index.jsx
@@ -7,18 +7,18 @@ export default function Login() {
       <div className="p-5 bg-login h-screen text-center overflow-y-auto flex flex-wrap items-center justify-center">
          <div className='top-area flex justify-center flex-wrap flex-column items-center'>
             <div className='w-full'>
-               <img src={SplashIcon} className="m-auto h-20" />
+               <img src={SplashIcon} className="m-auto h-16" />
             </div>
-            <h3 className="py-4 text-pink text-18 leading-24 font-semibold">Finance <span className="text-purple">Manager</span></h3>
-            <h2 className="text-18 leading-24 font-semibold">Seamlessly manage your Finances</h2>
-            <p className="pt-3 pb-6 text-14 leading-24 text-textColor">Discover the revolutionary semi automated financial management experience.</p>
+            <h3 className="pt-2 pb-3 text-pink text-14 leading-18 font-semibold">Finance <span className="text-purple">Manager</span></h3>
+            <h2 className="text-16 leading-20 font-semibold">Seamlessly manage your Finances</h2>
+            <p className="pt-2 pb-4 text-14 leading-20 text-textColor">Discover the revolutionary semi automated financial management experience.</p>
             <div className='w-full'>
-               <Link to='/home' className='flex items-center justify-center py-3 px-6 bg-white rounded-full font-semibold text-13 leading-20 mb-4 w-full'>
+               <Link to='/home' className='flex items-center justify-center py-3 px-6 bg-white rounded-full font-semibold text-13 leading-20 mb-2 w-4/6 mx-auto'>
                   <img src={Google} className='mr-2' />Continue with Google
                </Link>
             </div>
 
-            <img src={FinanceManager} className='mx-auto h-60' style={{ minHeight: '240px' }} />
+            <img src={FinanceManager} className='mx-auto h-48' style={{ minHeight: '192px' }} />
          </div>
       </div>
    );
